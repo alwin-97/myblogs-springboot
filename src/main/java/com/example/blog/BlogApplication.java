593e17189358266c0e26ff30efc51a6f3bdd9254
alwin-97 <alwinjosephkaduppil@gmail.com>
2026-04-29 11:26:36 +0530

    adding data base connectivity basics - configuration, data model and repository

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -6,6 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
 @SpringBootApplication
 public class BlogApplication {
 
@@ -15,9 +24,63 @@ public class BlogApplication {
 
 }
 
+
+@Entity
+@Table(name = "blogs")
+class Blog {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String content;
+
+	Blog(){}
+
+	Blog(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
+
+interface BlogRepository extends JpaRepository<Blog, Long> {
+	// JpaRepository provides basic CRUD operations
+}
+
 @Controller
 @RequestMapping()
 class BlogController {
+
+	private final BlogRepository blogRepository;
+
+	public BlogController(BlogRepository blogRepository) {
+		this.blogRepository = blogRepository;
+	}
+
 
 	@GetMapping("/")
 	public String home() {
